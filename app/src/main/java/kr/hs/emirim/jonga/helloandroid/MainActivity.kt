57f63bcdfd3ba563185f343df6caec066154ab50
Jonga Gang <whddk0209@gmail.com>
2020-09-17 20:41:53 +0900

    Import Project

---
+++ app/src/main/java/kr/hs/emirim/jonga/helloandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.jonga.helloandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
